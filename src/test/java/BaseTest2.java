720b35d890e1ddc19036cf1976d691c8ca41078c
eslamlotfy213 <eslamlotfy213@yahoo.com>
2022-12-02 03:39:26 +0200

    userY add function

---
+++ src/test/java/BaseTest2.java
@@ -0,0 +1,21 @@
+public class BaseTest2 {
+
+
+
+    public  void  add1(){
+
+    }
+
+
+    public  void  add2(){
+
+    }
+
+    public  void  add3(){
+
+    }
+
+    public  void  add4(){
+
+    }
+}
